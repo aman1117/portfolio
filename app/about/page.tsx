aa5ebf65813bcec76fcebb45bf37ecda9885a1a7
Aman <arl0817osho@gmail.com>
2024-07-13 06:32:10 +0530

    made correct escape character in about/page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,49 +6,48 @@ const AboutMe: React.FC = () => {
       <section className="mb-4">
         <h2 className="text-4xl font-bold mb-3">About</h2>
         <p className="text-lg leading-5">
-        I'm Aman Sharma, a Computer Science & Engineering student at NSUT, Delhi.
+          I&apos;m Aman Sharma, a Computer Science & Engineering student at NSUT, Delhi.
         </p>
         <p className="text-lg leading-5 my-4">
-        I'm passionate about software development, with a focus on full-stack web projects and competitive programming.
+          I&apos;m passionate about software development, with a focus on full-stack web projects and competitive programming.
         </p>
         <p className="text-lg leading-5 my-4">
-        I've built applications like ShareCode and Growing Buddies, and<br/> I'm proficient in C++, Golang, React, Nextjs and various web technologies.
+          I&apos;ve built applications like ShareCode and Growing Buddies, and<br/> I&apos;m proficient in C++, Golang, React, Nextjs and various web technologies.
         </p>
         <p className="text-lg leading-5">
-        Here's a copy of my <a href="/Aman.pdf" className="text-indigo-900 underline" download>resume.</a>
+          Here&apos;s a copy of my <a href="/Aman.pdf" className="text-indigo-900 underline" download>resume.</a>
         </p>
       </section>
 
       <section className="mb-4">
-      <h2 className="text-2xl font-bold mb-1">Interests</h2>
+        <h2 className="text-2xl font-bold mb-1">Interests</h2>
         <p className="text-lg leading-5">
-        I enjoy graphic design, philosophical literature, origami, and calligraphy. I lead the design team for my university's fine arts society. Here's some<span className="text-indigo-900 underline"> Stuff I Like</span>
+          I enjoy graphic design, philosophical literature, origami, and calligraphy. I lead the design team for my university&apos;s fine arts society. Here&apos;s some<span className="text-indigo-900 underline"> Stuff I Like</span>
         </p>
       </section>
 
       <section className="mb-4">
-      <h2 className="text-2xl font-bold mb-1">Find me online</h2>
-      <div className="flex ml-8">
+        <h2 className="text-2xl font-bold mb-1">Find me online</h2>
+        <div className="flex ml-8">
           <ul className="list-disc list-inside text-lg leading-5">
             <li>
-              I am <span  className="text-indigo-900 underline">aman1117</span> on GitHub
+              I am <span className="text-indigo-900 underline">aman1117</span> on GitHub
             </li>
             <li>
-              I am <span  className="text-indigo-900 underline">aman1117</span> on Leetcode
+              I am <span className="text-indigo-900 underline">aman1117</span> on Leetcode
             </li>
             <li>
-              I am <span  className="text-indigo-900 underline">aman1117</span> on LinkedIn
+              I am <span className="text-indigo-900 underline">aman1117</span> on LinkedIn
             </li>
-            
             <li>
-              I am <span  className="text-indigo-900 underline underline-offset-2">_aman1117_</span> on Instagram
+              I am <span className="text-indigo-900 underline underline-offset-2">_aman1117_</span> on Instagram
             </li>
           </ul>
         </div>
       </section>
 
       <section className="mb-4">
-      <h2 className="text-2xl font-bold mb-1">The Site</h2>
+        <h2 className="text-2xl font-bold mb-1">The Site</h2>
         <p className="text-lg leading-5 ">
           This site is written in React18 using Typescript and Tailwind and Nextjs as framework.<br/> Currently, I am hosting this site on Vercel. The site is deployed to Vercel automatically as<br/> I push to GitHub.
         </p>
