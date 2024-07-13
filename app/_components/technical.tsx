323fcffa9c4ec16b96b9a896248c1626c1225247
Aman <arl0817osho@gmail.com>
2024-07-13 17:59:32 +0530

    refactor: Update margin and spacing in technical skill component

--- app/_components/technical.tsx
+++ app/_components/technical.tsx
@@ -4,26 +4,30 @@ import React from 'react';
 const TechnicalSkill: React.FC = () => {
   const skills: { [key: string]: string[] } = {
     'Programming Languages': ['Problem Solving,', 'C++,', 'Golang,', 'Python'],
-    'Web Development': ['HTML/CSS,', 'TypeScript,', 'ReactJS,', 'Node.js,', 'NextJS'],
-    'Databases': ['MySQL,', 'Google Firebase,', 'MongoDB,', 'PostgreSQL'],
-    'Version Control': ['Git,', 'GitHub,'],
+    'Web Development': [ 'HTML/CSS','TypeScript,', 'ReactJS,', 'Node.js,', 'NextJS'],
+    'Databases': ['MySQL,', ' Google Firebase,', 'MongoDB,', 'PostgreSQL'],
+    'Version Control': ['Git,', 'GitHub'],
     'Coursework': ['OOPS,', 'DBMS,', 'OS,', 'CN'],
   };
 
   return (
     <div className="max-w-2xl mx-auto mt-3 mb-4">
-      <h2 className="text-4xl font-bold mb-2">Technical Skills</h2>
-      {Object.keys(skills).map((category, index) => (
-        <div key={index} className=" flex items-center ">
-            <Dot size={32} className="inline-block -mb-1" />
-          <h3 className="md:text-lg text-xs font-semibold md:mr-4 mr-2 -mb-1">{category}:</h3>
-
-            {skills[category].map((skill, i) => (
-              <p key={i} className= "md:text-lg text-xs md:mr-2 mr-1 -mb-1">{skill}</p>
-            ))}
-
-        </div>
-      ))}
+      <h2 className="md:text-4xl text-4xl font-semibold mb-3"> Technical Skills</h2>
+      {
+        Object.keys(skills).map((skill, index) => (
+          <div key={index} className="flex items-start mb-2">
+            <Dot size={32} className="inline-block" />
+            <div>
+              <h3 className="text-black md:text-lg md:leading-5 leading-tight font-semibold">
+                {skill}
+              </h3>
+              <p className="text-black md:text-lg text-sm md:leading-5 leading-tight">
+                {skills[skill].join(' ')}
+              </p>
+            </div>
+          </div>
+        ))
+      }
     </div>
   );
 };
