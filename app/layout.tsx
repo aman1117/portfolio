f22380e83df4cc539823b2faad7a3573648fc348
Aman <arl0817osho@gmail.com>
2024-07-13 17:22:01 +0530

    refactor: Remove unused font-family declaration in CSS and layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,11 +37,9 @@ export default function RootLayout({
 }>) {
   return (  
     <html lang="en">
-      <body className={` px-3` } style={
-        {
-          fontFamily: 'SourceSerifPro_600SemiBold',
-        }
-      }>
+      <body className={` px-3` } style={{
+        fontFamily: 'SourceSerifPro_600SemiBold',
+      }}>
         <div className="flex flex-col min-h-screen" >
           <div className="flex-grow">
             <Header />
