1222254d8fad029442a53797732dbf38eed673f4
Aman <arl0817osho@gmail.com>
2024-07-15 00:52:27 +0530

    refactor: Update margin and spacing in interests page component

--- app/about/interests/page.tsx
+++ app/about/interests/page.tsx
@@ -4,7 +4,7 @@ const Interests: React.FC = () => {
     return (
         <div className="max-w-2xl mx-auto mt-3 mb-4 ">
             <h1 className="text-4xl font-bold mb-4">Interests</h1>
-            <ul className="list-disc pl-5 space-y-3">
+            <ul className="list-disc pl-5 space-y-1">
                 <li className="md:text-lg">
                     <strong>Graphic Design:</strong> I want to learn digital painting and really like creative photo editing.
                 </li>
