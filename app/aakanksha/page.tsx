f0910f391f1fd4066b62f641309ddc51d7f09f73
Aman <arl0817osho@gmail.com>
2024-07-26 09:26:38 +0530

    refactor: Update Aakanksha page to use lazy loading for image

--- app/aakanksha/page.tsx
+++ app/aakanksha/page.tsx
@@ -1,11 +1,17 @@
 import Image from "next/image";
 
 const Aakanksha = () => {
-    return (
-        <div className="flex items-center justify-center ">
-            <Image src="/aakanksha.jpeg" alt="Aakanksha" width={670} height={400} className="md:grayscale hover:filter-none transition-all" />
-        </div>
-    );
-}
- 
+  return (
+    <div className="flex items-center justify-center ">
+      <Image
+        src="/aakanksha.jpeg"
+        alt="Aakanksha"
+        width={670}
+        height={400}
+        className="md:grayscale hover:filter-none transition-all"
+      />
+    </div>
+  );
+};
+
 export default Aakanksha;
