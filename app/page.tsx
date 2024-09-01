cc593be1ce1d995a338939c1ed895d6bc8edc3d2
aman1117 <arl0817osho@gmail.com>
2024-09-01 11:05:45 +0000

    added resume and knight

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,22 @@ import TechnicalSkill from "./_components/technical";
 
 export default function Home() {
   return (
-      <div className="flex flex-col w-full h-full py-4 max-w-2xl mx-auto ">
-        <div>
-        <p className="  text-black md:text-lg md:leading-5 leading-tight ">
-        This is the homepage of Aman Sharma, a 4th year BTech CSE student.<br/> Experienced in backend (Go) and frontend (React.js, Next.js) development.
+    <div className="flex flex-col w-full h-full py-4 max-w-2xl mx-auto">
+      <div>
+        <p className="text-black md:text-lg md:leading-5 leading-tight">
+          This is the homepage of Aman Sharma, a 4th year BTech CSE student.<br />
+          Knight at LeetCode with a passion for competitive programming.
+          <br />
+          <br />
+          Experienced in backend (Go) and frontend (React.js, Next.js) development.
         </p>
-        </div>
-          <div className=" mt-4">
-        <ProjectsList />
-          </div>
-          <div className="mt-2">
-        <TechnicalSkill />
-          </div>
       </div>
+      <div className="mt-4">
+        <ProjectsList />
+      </div>
+      <div className="mt-2">
+        <TechnicalSkill />
+      </div>
+    </div>
   );
 }
