605363e2703f4288eaa446caafe6d0fef36fe64d
Aman <arl0817osho@gmail.com>
2025-05-05 13:39:02 +0530

    style: Adjust layout and spacing in experience section of contact page

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -4,11 +4,11 @@ const ContactPage: React.FC = () => {
     return (
         <div className="max-w-2xl mx-auto mt-2 mb-10 w-full h-full flex flex-col">
             <p className="text-4xl font-bold mb-3 mr-1">Experience</p>
-            <div className="mt-4">
+            <div >
                 <div >
                     <h2 className="text-2xl font-semibold mb-2">Shipsy, Software Engineer Intern</h2>
-                    <p className="text-sm  mb-2 italic"><strong>Jan 2025 - Present</strong></p>
-                    <p className="text-sm  mb-2 italic"><strong> Gurgaon, onsite</strong></p>
+                    <p className="text-sm   italic"><strong>Jan 2025 - Present</strong></p>
+                    <p className="text-sm  mb-4 italic"><strong> Gurgaon, onsite</strong></p>
                     <ul className="list-disc list-inside mt-2 space-y-1 text-sm">
                         <li className='mb-2 text-black md:text-lg md:leading-5 leading-tight'>
                             Developed a comprehensive finance mobile web view UI for <strong>QuipUp</strong>, integrating APIs for riders to access their financial information with adaptable client-specific configurations.
