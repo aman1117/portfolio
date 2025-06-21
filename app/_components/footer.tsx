419092020dd2dcd99ce3e0aae92e4b75b9cf4bc1
Aman <arl0817osho@gmail.com>
2025-06-21 18:23:53 +0530

    update: Wrap footer name in a link for improved navigation

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,9 +1,15 @@
+import Link from "next/link";
+
 const Footer = () => {
   return (
     <div className="border-t-2 mx-auto w-full max-w-2xl flex items-center justify-center m-2">
-      <p className=" text-lg text-indigo-900 underline underline-offset-2">
-        aman.sharma
-      </p>
+      <div className="flex items-center justify-center">
+        <Link href="/">
+          <p className=" text-lg text-indigo-900 underline underline-offset-2">
+            aman.sharma
+          </p>
+        </Link>
+      </div>
     </div>
   );
 };
